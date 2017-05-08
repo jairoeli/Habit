c1e9afcbfbd0176101a82fa8817e8ced7309a4bd
jairoeli <jairoelid@gmail.com>
2017-05-08 11:27:52 -0500

    Add Task Service

---
+++ Zero/Sources/Services/TaskService.swift
@@ -0,0 +1,141 @@
+//
+//  TaskService.swift
+//  Zero
+//
+//  Created by Jairo Eli de Leon on 5/8/17.
+//  Copyright © 2017 Jairo Eli de León. All rights reserved.
+//
+
+import RxSwift
+
+enum TaskEvent {
+  case create(Task)
+  case update(Task)
+  case delete(id: String)
+  case markAsDone(id: String)
+  case markAsUnDone(id: String)
+}
+
+protocol TaskServiceType {
+  var event: PublishSubject<TaskEvent> { get }
+  func fetchTask() -> Observable<[Task]>
+
+  @discardableResult
+  func saveTasks(_ tasks: [Task]) -> Observable<Void>
+
+  func create(title: String, memo: String?) -> Observable<Task>
+  func update(taskID: String, title: String, memo: String?) -> Observable<Task>
+  func delete(taskID: String) -> Observable<Task>
+  func markAsDone(taskID: String) -> Observable<Task>
+  func markAsUnDone(taskID: String) -> Observable<Task>
+}
+
+final class TaskService: BaseService, TaskServiceType {
+
+  let event = PublishSubject<TaskEvent>()
+
+  func fetchTask() -> Observable<[Task]> {
+    if let savedTaskDictionaries = self.provider.userDefaultsService.value(forKey: .tasks) {
+      let tasks = savedTaskDictionaries.flatMap(Task.init)
+      return .just(tasks)
+    }
+
+    let defaultTasks: [Task] = [
+      Task(title: "Walk 3000 steps"),
+      Task(title: "Read 10 chapters of book"),
+      Task(title: "Drink 3 glasses of water")
+    ]
+
+    let defaultsTaskDictionaries = defaultTasks.map { $0.asDictionary() }
+    self.provider.userDefaultsService.set(value: defaultsTaskDictionaries, forKey: .tasks)
+    return .just(defaultTasks)
+  }
+
+  @discardableResult
+  func saveTasks(_ tasks: [Task]) -> Observable<Void> {
+    let dicts = tasks.map { $0.asDictionary() }
+    self.provider.userDefaultsService.set(value: dicts, forKey: .tasks)
+    return .just(Void())
+  }
+
+  func create(title: String, memo: String?) -> Observable<Task> {
+    return self.fetchTask()
+      .flatMap { [weak self] tasks -> Observable<Task> in
+        guard let `self` = self else { return .empty() }
+        let newTask = Task(title: title, memo: memo)
+        return self.saveTasks(tasks + [newTask]).map { newTask }
+    }
+    .do(onNext: { task in
+      self.event.onNext(.create(task))
+    })
+  }
+
+  func update(taskID: String, title: String, memo: String?) -> Observable<Task> {
+    return self.fetchTask()
+      .flatMap { [weak self] tasks -> Observable<Task> in
+        guard let `self` = self else { return .empty() }
+        guard let index = tasks.index(where: { $0.id == taskID }) else { return .empty() }
+        var tasks = tasks
+        let newTask = tasks[index].with {
+          $0.title = title
+          $0.memo = memo
+        }
+        tasks[index] = newTask
+        return self.saveTasks(tasks).map { newTask }
+    }
+      .do(onNext: { task in
+        self.event.onNext(.update(task))
+      })
+  }
+
+  func delete(taskID: String) -> Observable<Task> {
+    return self.fetchTask()
+      .flatMap { [weak self] tasks -> Observable<Task> in
+        guard let `self` = self else { return .empty() }
+        guard let index = tasks.index(where: { $0.id == taskID }) else { return .empty() }
+        var tasks = tasks
+        let deletedTasks = tasks.remove(at: index)
+        return self.saveTasks(tasks).map { deletedTasks }
+    }
+      .do(onNext: { task in
+        self.event.onNext(.delete(id: task.id))
+      })
+  }
+
+  func markAsDone(taskID: String) -> Observable<Task> {
+    return self.fetchTask()
+      .flatMap { [weak self] tasks -> Observable<Task> in
+        guard let `self` = self else { return .empty() }
+        guard let index = tasks.index(where: { $0.id == taskID }) else { return .empty() }
+        var tasks = tasks
+        let newTask = tasks[index].with {
+          $0.isDone = true
+          return
+        }
+        tasks[index] = newTask
+        return self.saveTasks(tasks).map { newTask }
+    }
+      .do(onNext: { task in
+        self.event.onNext(.markAsDone(id: task.id))
+      })
+  }
+
+  func markAsUnDone(taskID: String) -> Observable<Task> {
+    return self.fetchTask()
+      .flatMap { [weak self] tasks -> Observable<Task> in
+        guard let `self` = self else { return .empty() }
+        guard let index = tasks.index(where: { $0.id == taskID }) else { return .empty() }
+        var tasks = tasks
+        let newTask = tasks[index].with {
+          $0.isDone = false
+          return
+        }
+        tasks[index] = newTask
+        return self.saveTasks(tasks).map { newTask }
+    }
+      .do(onNext: { task in
+        self.event.onNext(.markAsUnDone(id: task.id))
+      })
+  }
+
+}
