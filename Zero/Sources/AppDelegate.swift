bfaabf28e02e45cf3561865d4e04c8639fd1ca51
jairoeli <jairoelid@gmail.com>
2017-05-08 10:00:44 -0500

    Add SwiftLint

--- Zero/Sources/AppDelegate.swift
+++ Zero/Sources/AppDelegate.swift
@@ -13,17 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
     window = UIWindow(frame: UIScreen.main.bounds)
     window?.backgroundColor = .red
     window?.makeKeyAndVisible()
     window?.rootViewController = UIViewController()
-    
+
     return true
   }
 
-
 }
-
