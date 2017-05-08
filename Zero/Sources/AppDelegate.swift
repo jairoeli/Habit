a93872c44815504a849685b72f10eb29fdbfb7af
jairoeli <jairoelid@gmail.com>
2017-05-08 09:49:18 -0500

    Initial commit

---
+++ Zero/Sources/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Zero
+//
+//  Created by Jairo Eli de Leon on 5/8/17.
+//  Copyright © 2017 Jairo Eli de León. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.backgroundColor = .red
+    window?.makeKeyAndVisible()
+    window?.rootViewController = UIViewController()
+    
+    return true
+  }
+
+
+}
+
