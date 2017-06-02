7ef154693449873a689403ed42cb3bfede3b9f4c
jairoeli <jairoelid@gmail.com>
2017-06-01 22:38:19 -0500

    Add UITableView+Rx

---
+++ Zero/Sources/Rx/UITableView+Rx.swift
@@ -0,0 +1,23 @@
+//
+//  UITableView+Rx.swift
+//  Zero
+//
+//  Created by Jairo Eli de Leon on 6/1/17.
+//  Copyright © 2017 Jairo Eli de León. All rights reserved.
+//
+
+import RxCocoa
+import RxDataSources
+import RxSwift
+
+extension Reactive where Base: UITableView {
+
+  func itemSelected<S: SectionModelType>(dataSource: TableViewSectionedDataSource<S>) -> ControlEvent<S.Item> {
+    let source = self.itemSelected.map { indexPath in
+      dataSource[indexPath]
+    }
+
+    return ControlEvent(events: source)
+  }
+  
+}
