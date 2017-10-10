71f22548bd20bbfeb8117036493a35bbc2d78def
jairoeli <jairoelid@gmail.com>
2017-10-09 23:09:16 -0500

    Add year progress bar

--- Zero/Sources/Views/SectionHeaderView.swift
+++ Zero/Sources/Views/SectionHeaderView.swift
@@ -19,26 +19,42 @@ final class SectionHeaderView: UIView {
 
   // MARK: - UI
 
-  lazy var titleLabel = UILabel() <== {
-    $0.text = TimeOfTheDay.getGreetingFromTheCurrentOfTheDay()
+  lazy var headerView = UIView() <== {
+    $0.backgroundColor = .snow
+    $0.layer.cornerRadius = 10
+    $0.layer.borderWidth = 1
+    $0.layer.borderColor = UIColor.platinumBorder.cgColor
+  }
+  lazy var yearLabel = UILabel() <== {
+    $0.text = "YEAR"
     $0.textColor = .charcoal
-    $0.font = .black(size: 32)
+    $0.font = .bold(size: 18)
   }
 
-  lazy var displayDate = UILabel() <== {
-    let date = Date()
-    $0.text =  date.currentDate()
-    $0.textColor = .midGray
-    $0.font = .footnote()
+  lazy var progressLabel = UILabel() <== {
+    $0.text = "PROGRESS"
+    $0.textColor = .charcoal
+    $0.font = .bold(size: 18)
+  }
+
+  lazy var percentLabel = UILabel() <== {
+    $0.text = CalculateProgress.percent()
+    $0.textColor = .charcoal
+    $0.font = .bold(size: 42)
+  }
+
+  lazy var progessResult = UILabel() <== {
+    $0.text = CalculateProgress.result()
+    $0.font = .bold(size: 14)
   }
 
   // MARK: - Initializing
 
   override init(frame: CGRect) {
     super.init(frame: frame)
+    self.translatesAutoresizingMaskIntoConstraints = false
     self.backgroundColor = .snow
-
-    let subviews: [UIView] = [titleLabel, displayDate]
+    let subviews: [UIView] = [headerView, yearLabel, progressLabel, percentLabel, progessResult]
     self.add(subviews)
     self.setUpView()
   }
@@ -50,34 +66,35 @@ final class SectionHeaderView: UIView {
   // MARK: - Layout
 
   fileprivate func setUpView() {
-    self.titleLabel.snp.makeConstraints { make in
-      make.top.equalTo(self.displayDate.snp.bottom)
-      make.leading.equalTo(Metric.leftPadding)
+    self.headerView.snp.makeConstraints { (make) in
+      make.edges.equalToSuperview()
     }
 
-    self.displayDate.snp.makeConstraints { make in
-      if #available(iOS 11.0, *), iPhoneX() == true {
-        make.top.equalTo(self.safeAreaLayoutGuide.snp.topMargin).offset(8)
-      } else {
-        make.top.equalTo(Metric.cellPadding)
-      }
-      make.leading.equalTo(Metric.leftPadding)
+    self.yearLabel.snp.makeConstraints { (make) in
+      make.top.equalTo(12)
+      make.leading.equalTo(12)
+    }
+
+    self.progressLabel.snp.makeConstraints { (make) in
+      make.top.equalTo(self.yearLabel.snp.bottom)
+      make.leading.equalTo(12)
+    }
+
+    self.percentLabel.snp.makeConstraints { make in
+      make.top.equalTo(9)
+      make.trailing.equalTo(-12)
+    }
+
+    self.progessResult.snp.makeConstraints { (make) in
+      make.top.equalTo(self.progressLabel.snp.bottom).offset(12)
+      make.leading.equalTo(14)
     }
   }
 
   // MARK: - Size
 
   override var intrinsicContentSize: CGSize {
-    if iPhoneX() == true {
-      return CGSize(width: self.width, height: 110)
-    } else {
-      return CGSize(width: self.width, height: 100)
-    }
-  }
-
-  func iPhoneX() -> Bool {
-    let screenRect: CGRect = UIScreen.main.bounds
-    return max(screenRect.size.height, screenRect.size.width) == 812
+    return CGSize(width: self.width, height: 100)
   }
 
 }
